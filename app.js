fcdd56551e26baff5d2396a4bd2d680879c03ba5
Jimmy Yang <jimmyy1337@gmail.com>
2022-03-12 17:37:16 -0500

    End of REST, install GraphQL

--- app.js
+++ app.js
@@ -7,9 +7,6 @@ const mongoose = require("mongoose");
 const multer = require("multer");
 const { v4: uuidv4 } = require("uuid");
 
-const feedRoutes = require("./routes/feed");
-const authRoutes = require("./routes/auth");
-
 const app = express();
 
 const fileStorage = multer.diskStorage({
@@ -50,9 +47,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use("/feed", feedRoutes);
-app.use("/auth", authRoutes);
-
 app.use((error, req, res, next) => {
   console.log(error);
   const status = error.statusCode || 500;
@@ -66,8 +60,6 @@ mongoose
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.02xhr.mongodb.net/messages?retryWrites=true&w=majority`
   )
   .then(() => {
-    const server = app.listen(8080);
-    const io = require("./socket").init(server);
-    io.on("connection", (socket) => {});
+    server = app.listen(8080);
   })
   .catch((err) => console.log(err));
